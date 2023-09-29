337cecb25d60a96794ee5ec6b1f9683cb28d65ab
Merykson <merykson.acacio@hotmail.com>
2023-09-29 19:24:05 -0300

    feat: initial project #main

---
+++ src/main/java/com/card/challenge/CardChallengeBackApplication.java
@@ -0,0 +1,13 @@
+package com.card.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardChallengeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardChallengeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/card/challenge/CardChallengeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.card.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardChallengeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
